4d8b7cf3961a7a84722c9ae0887506af4572249e
sayi <isaiah.rivasc@proton.me>
2026-09-24 11:08:45 -0600

    Creacion de funcion de minado y cración de metodos

--- alg_mineria.py
+++ alg_mineria.py
@@ -19,30 +19,27 @@ class Bloque:
             str(self.timestamp) +
             str(self.nonce)
         )
+        return encabezado
 
-        return encabezado.encode()
+    def modificar_nonce(self,incoming_nonce):
+        self.nonce = incoming_nonce
 
-def minar_bloque_2(encabezado, target, nonce_max):
-    bloque_hash = hashlib.sha256(hashlib.sha256(encabezado))
+    def modificar_timestamp(self,incoming_timestamp):
+        self.timestamp = incoming_timestamp
+
+    def obtener_nonce(self):
+        return self.nonce
+
+def minar_bloque(bloque, target, nonce_max):
+    bloque_encabezado = bloque.obtener_encabezado
+    bloque_hash = hashlib.sha256(hashlib.sha256(bloque))
 
     if bloque_hash < target:
-        return encabezado, bloque_hash
+        return bloque_encabezado, bloque_hash
+    bloque.modificar_nonce += 1
 
-    if nonce > nonce_max:
-        encabezado
+    if bloque.obtener_nonce > nonce_max:
+        bloque.modificar_timestamp = time.time()
+        bloque.modificar_nonce = 0
 
     
-
-def minarbloque(transacciones, hash_bloque_anterior, target):
-
-
-    while True:
-        hash_resultado = hashlib.sha256(hashlib.sha256(encabezado))
-        if hash_resultado < target : 
-            return encabezado, hash_resultado
-        encabezado.nonce+=1
-
-        if encabezado.nonce > nonce_max :
-            encabezado.timestamp = time.time()
-            encabezado.nonce=0
-
