16905f7042391a10eaec27e81bfd5bd85052529a
VDBPR <darybasurto04@gmail.com>
2026-09-24 10:56:24 -0600

    Se creó clase Bloque

--- alg_mineria.py
+++ alg_mineria.py
@@ -3,7 +3,24 @@ import time
 
 # Definir clase 
 
+class Bloque:
 
+    def __init__(self, transacciones, hash_anterior, timestamp, nonce=0):
+        self.transacciones = transacciones
+        self.hash_anterior = hash_anterior
+        self.timestamp = timestamp
+        self.nonce = nonce
+
+    def obtener_encabezado(self):
+
+        encabezado = (
+            str(self.transacciones) +
+            str(self.hash_anterior) +
+            str(self.timestamp) +
+            str(self.nonce)
+        )
+
+        return encabezado.encode()
 
 def minar_bloque_2(encabezado, target, nonce_max):
     bloque_hash = hashlib.sha256(hashlib.sha256(encabezado))
