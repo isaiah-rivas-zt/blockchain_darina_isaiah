678a530938f6a738b898b3f7056033447612f8fe
sayi <isaiah.rivasc@proton.me>
2026-09-24 11:14:41 -0600

    Modificacion de minar_bloque

--- alg_mineria.py
+++ alg_mineria.py
@@ -21,25 +21,17 @@ class Bloque:
         )
         return encabezado
 
-    def modificar_nonce(self,incoming_nonce):
-        self.nonce = incoming_nonce
-
-    def modificar_timestamp(self,incoming_timestamp):
-        self.timestamp = incoming_timestamp
-
-    def obtener_nonce(self):
-        return self.nonce
-
 def minar_bloque(bloque, target, nonce_max):
-    bloque_encabezado = bloque.obtener_encabezado
+    bloque_encabezado = bloque.obtener_encabezado()
     bloque_hash = hashlib.sha256(hashlib.sha256(bloque))
 
-    if bloque_hash < target:
-        return bloque_encabezado, bloque_hash
-    bloque.modificar_nonce += 1
+    while(True):
+        if bloque_hash < target:
+            return bloque_encabezado, bloque_hash
+        bloque.nonce += 1
 
-    if bloque.obtener_nonce > nonce_max:
-        bloque.modificar_timestamp = time.time()
-        bloque.modificar_nonce = 0
+        if bloque.nonce > nonce_max:
+            bloque.timestamp = time.time()
+            bloque.nonce = 0
 
     
